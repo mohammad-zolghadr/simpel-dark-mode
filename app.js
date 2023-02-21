dc59063457d963c1061124e47c230605589fff90
Mohammad Zolghadr <mohammad.zol9978@gmail.com>
2023-02-21 20:09:40 +0330

    Code Cleaned

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
-const checkBox = document.querySelector("#toggle");
-let htmlElement = document.documentElement;
+const toggle = document.querySelector("#toggle");
+const htmlElement = document.documentElement;
 
-checkBox.addEventListener("click", () => {
-  if (checkBox.checked) {
-    htmlElement.setAttribute("data-theme", "dark");
-  } else {
-    htmlElement.setAttribute("data-theme", "light");
-  }
+toggle.addEventListener("click", (e) => {
+  if (e.target.checked) htmlElement.setAttribute("data-theme", "dark");
+  else htmlElement.setAttribute("data-theme", "light");
 });
